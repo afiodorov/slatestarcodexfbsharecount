7a30cf5e1a12db3019cceaf7e436a2a617cd2e22
Tom Artiom Fiodorov <a.fiodorov@yahoo.co.uk>
2015-09-15 12:16:23 +0100

    first attempt

--- fb_shares.js
+++ fb_shares.js
@@ -1,25 +1,47 @@
+var Promise = require('promise');
+
 var links = require('./links');
-var jsdom = require('jsdom');
-var localSotrage = require('node-storage-shim');
 
-links.links.forEach(function(link) {
-  jsdom.env({
-    url: link,
-    features: {
-      FetchExternalResources: ['script'],
-      ProcessExternalResources: ['script'],
-      SkipExternalResources: 'https:\/\/bakkot\.github\.io\/SlateStarComments\/ssc\.js'
-    },
-    done: function(errors, win) {
-      if (errors) {
-        console.log('error ' + link);
-        console.log(errors);
-        return;
-      }
-      console.log(win.document.body.innerHTML);
-      console.log(win.document.getElementsByClassName('share-count'));
-    }
+var phantom = require('phantom');
+
+var options = {
+  'load-images': 'no'
+};
+
+var createCountPromise = function(link) {
+  return new Promise(function(resolve, reject) {
+    phantom.create(function (ph) {
+      ph.createPage(function (page) {
+        page.open(link, function (status) {
+          page.evaluate(function() {
+            try {
+              return document.getElementsByClassName('share-count')[1].innerHTML.toString();
+            } catch(ignore) {}
+
+            return 0;
+          }, function (result) {
+            resolve(result);
+            ph.exit();
+          });
+        });
+      });
+    }, options);
   });
+};
 
+var promisedCounts = [];
+
+function afterIth(i, ithResult) {
+  console.log(links.links[i] + ',' + ithResult);
+  if (i + 1 < links.links.length) {
+      promisedCounts[i + 1] = createCountPromise(links.links[i + 1]);
+      promisedCounts[i + 1].then(function(count) {
+      afterIth(i + 1, count).call();
+    });
+  }
+}
+
+promisedCounts[0] = createCountPromise(links.links[0]);
+promisedCounts[0].then(function(count) {
+  afterIth(0, count).call();
 });
-
